45256b7d649f364c9a7ab20b6b1ebbd8d7acfa6b
agnieszka.chrzanowska@vimanet.com <agnieszka.chrzanowska@vimanet.com>
2024-01-06 15:34:05 +0100

    Added example of interface and service

---
+++ src/pl/dc4b/cardirectory/dao/CalcDao.java
@@ -0,0 +1,6 @@
+package pl.dc4b.cardirectory.dao;
+
+public interface CalcDao {
+    int addTwoNumbers(int x, int y);
+}
+
---
+++ src/pl/dc4b/cardirectory/dao/CalcDaoImpl.java
@@ -0,0 +1,8 @@
+package pl.dc4b.cardirectory.dao;
+
+public class CalcDaoImpl implements CalcDao {
+    @Override
+    public int addTwoNumbers(int x, int y) {
+        return x + y;
+    }
+}
